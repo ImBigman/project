739cebd0f48a1aef19b389de857c33776f20677b
ImBigman <b1gmanever@gmail.com>
2020-05-19 15:01:21 +0300

    fix find fresh episode service for different episode title

--- app/services/find_fresh_episode_service.rb
+++ app/services/find_fresh_episode_service.rb
@@ -7,10 +7,18 @@ class FindFreshEpisodeService
     document = Nokogiri::HTML(html)
     new_episode = document.css('.yt-lockup-dismissable')[0]
     title_node =  new_episode.children[3].children[1].children[0].text[1..-1].split('-')
+
+    if title_node.count == 1
+      title_node = new_episode.children[3].children[1].children[0].text[1..-1].split('|')
+      description = title_node[1..-1].join('|').strip
+    else
+      description = title_node[1].strip
+    end
+
     {
       thumbs: new_episode.css('img')[0]['src'],
       title: title_node[0].strip,
-      description: title_node[1].strip,
+      description: description,
       url: new_episode.css('a')[1]['href'][9..-1]
     }
   end
